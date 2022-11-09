6e53c227fb56c43050b752ee49d5d94e97673b8d
huyennk6 <huyennk6@fpt.edu.vn>
2022-11-10 00:38:29 +0700

    updated git

---
+++ MOB1014-IT18203/src/main/java/BAI_1_LAM_QUEN_JAVA/B1_ChuongTrinhDauTien.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI_1_LAM_QUEN_JAVA;
+
+/**
+ *
+ * @author Khanh Huyen
+ */
+public class B1_ChuongTrinhDauTien {
+
+    //c1: psvm+ tab
+    //c2: main +tab
+    public static void main(String[] args) {
+        //sout+tab
+        System.out.println("He s lo Ho s Ly Ly");
+        System.out.println("He s lo Ho s Ly Ly");
+        System.out.println("He s lo Ho s Ly Ly");
+        /*
+        - Shift+ F6: chạy chương trình
+        - Ctrl + /: đóng mở comment
+        - Alt + shift + F: format code
+        - Ctrl+ Shift + mũi tên lên xuống: copy code
+         */
+    }
+}
---
+++ MOB1014-IT18203/src/main/java/BAI_1_LAM_QUEN_JAVA/B2_KhaiBaoBien.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BAI_1_LAM_QUEN_JAVA;
+
+/**
+ *
+ * @author Khanh Huyen
+ */
+public class B2_KhaiBaoBien {
+
+    /*
+        1. Kiểu dữ liệu
+        boolean, byte, char, short, int, long, float, double
+        2. Khai báo biến
+            <kiểu dữ liệu> <tên biến> [=<giá trị>]
+        3. Quy tắc đặt tên biến
+           * Có 2 loại biến:
+            - Biến toàn cục: nằm ngoài Phương thức
+            - Biến cục bộ: nằm trong Phương thức
+           * Quy tắc đặt tên biến:
+            - Normal: firstname
+            - Camel Case: firstName => SinhVien (Pascal Case)
+            - Under score: first_name
+           * Lưu ý:
+            - ko dc bắt đầu bằng số
+            - ko trùng với keyword: int, if, else, return,...
+     */
+    //psvm+tab
+    int _bienToanCuc;// biến toàn cục
+    public static void main(String[] args) {
+        //biến cục bộ
+        int a = 5;
+        double diem = 9.5;
+        String name= "Khanh huyen";
+        
+    }
+}
